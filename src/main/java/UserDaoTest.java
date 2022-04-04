56d9ae93a8268748ff18681a23afc48f6de8e67d
20bum <20bum87@gmail.com>
2022-04-04 23:47:56 +0900

    add dao factory

--- src/main/java/UserDaoTest.java
+++ src/main/java/UserDaoTest.java
@@ -1,12 +1,31 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springbook.user.dao.ConnectionMaker;
 import springbook.user.dao.DConnectionMaker;
+import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
 
 public class UserDaoTest {
 
-    public static void main(String[] args) {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
-        UserDao dao = new UserDao(connectionMaker);
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
     }
 }
--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -7,6 +7,7 @@ import java.sql.*;
 public class UserDao {
 
     private ConnectionMaker connectionMaker;
+
     public UserDao(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
     }
